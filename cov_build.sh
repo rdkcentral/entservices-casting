a77cbfe36e8ae902bf012a3b5e603f5fefeafb25
agampa263 <akshaykumar_gampa@comcast.com>
2025-04-03 08:25:56 +0000

    test

--- cov_build.sh
+++ cov_build.sh
@@ -230,6 +230,7 @@ cmake -G Ninja -S entservices-casting -B build/entservices-casting \
   -DRDK_SERVICES_L1_TEST=ON \
   -DDS_FOUND=ON \
   -DPLUGIN_XCAST=ON \
+  -DPLUGIN_MIRACAST=ON
 
 
 cmake --build build/entservices-casting --target install
