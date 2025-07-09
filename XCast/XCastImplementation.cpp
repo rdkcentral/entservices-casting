960592344dfc5191e02e7166d34639b9075b36b2
ssitar583 <soundaryaa_sitaram@comcast.com>
2025-07-09 22:11:53 +0530

    Update XCastImplementation.cpp

--- XCast/XCastImplementation.cpp
+++ XCast/XCastImplementation.cpp
@@ -1,8 +1,8 @@
-/**
- * If not stated otherwise in this file or this component's LICENSE
- * file the following copyright and licenses apply:
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
  *
- * Copyright 2025 RDK Management
+ * Copyright 2024 RDK Management
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- **/
+ */
 
 #include "XCastImplementation.h"
 #include <sys/prctl.h>
