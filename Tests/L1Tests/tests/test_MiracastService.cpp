e500a368bf317bc144670d20992677f2be589e01
balav08 <54432605+balav08@users.noreply.github.com>
2025-07-22 14:52:07 +0530

    Update test_MiracastService.cpp

--- Tests/L1Tests/tests/test_MiracastService.cpp
+++ Tests/L1Tests/tests/test_MiracastService.cpp
@@ -916,6 +916,18 @@ TEST_F(MiracastServiceEventTest, P2P_GO_NEGOTIATION_FAIL_onClientConnectionError
 
 TEST_F(MiracastServiceEventTest, P2P_GO_FORMATION_FAIL_onClientConnectionError)
 {
+	if (std::filesystem::exists("/etc/device.properties")) {
+                  removeEntryFromFile("/etc/device.properties","WIFI_P2P_CTRL_INTERFACE=p2p0");
+        } else {
+        std::cout << "/etc/device.properties does not exist\n";
+        }
+ 
+        if (std::filesystem::exists("/var/run/wpa_supplicant/p2p0")) {
+               removeFile("/var/run/wpa_supplicant/p2p0");
+        } else {
+        std::cout << "/var/run/wpa_supplicant/p2p0 does not exist\n";
+        }
+	
 	createFile("/etc/device.properties","WIFI_P2P_CTRL_INTERFACE=p2p0");
 	createFile("/var/run/wpa_supplicant/p2p0","p2p0");
 
