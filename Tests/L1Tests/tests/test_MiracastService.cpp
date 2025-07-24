3cb55146172082c3c5e3d6c6927cbc79e0585080
Prathyushakothuru <142974139+Prathyushakothuru@users.noreply.github.com>
2025-07-24 14:55:59 +0530

    Update test_MiracastService.cpp

--- Tests/L1Tests/tests/test_MiracastService.cpp
+++ Tests/L1Tests/tests/test_MiracastService.cpp
@@ -55,7 +55,8 @@ namespace
 			printf("File %s successfully deleted\n", fileName);
 		}
 	}
-	#if 0
+        # Commented out the functions below because some test cases that use them have also been commented out. Since those test cases are disabled, these functions are now unused.
+	#if 0 
 	static void removeEntryFromFile(const char* fileName, const char* entryToRemove)
 	{
 		std::ifstream inputFile(fileName);
