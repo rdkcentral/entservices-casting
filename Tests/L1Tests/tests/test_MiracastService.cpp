ae57d81bf28f81370463009f1b4b99628daf4d5e
balav08 <54432605+balav08@users.noreply.github.com>
2025-08-10 12:51:05 +0530

    Update test_MiracastService.cpp

--- Tests/L1Tests/tests/test_MiracastService.cpp
+++ Tests/L1Tests/tests/test_MiracastService.cpp
@@ -219,7 +219,7 @@ protected:
 
 		TEST_LOG("Before destructor sleep ");
 		//Wait for all the previous destructor process to complete
-		//std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 		TEST_LOG("After destructor sleep ");
     }
 };
