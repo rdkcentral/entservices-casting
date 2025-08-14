3afbf8222703a50d6f306925db48692c42736fc6
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2025-08-14 17:49:34 +0530

    RDKEMW-5810 : Update MiracastLogger.h

--- Miracast/common/MiracastLogger.h
+++ Miracast/common/MiracastLogger.h
@@ -37,7 +37,10 @@ namespace MIRACAST {
  * It is essental to start with 0 and increase w/o gaps as the value
  * can be used for indexing in a mapping table.
  */
+#ifndef DEFAULT_LOG
+#define DEFAULT_LOG 
 enum LogLevel {FATAL_LEVEL = 0, ERROR_LEVEL, WARNING_LEVEL, INFO_LEVEL, VERBOSE_LEVEL, TRACE_LEVEL};
+#endif
 
 /**
  * @brief Init logging
