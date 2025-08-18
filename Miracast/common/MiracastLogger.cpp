7fe5bcf2d678e57f04ec0dc33c17efd6fd7b4b72
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2025-08-18 15:04:04 +0530

    RDKEMW-5810 : Update MiracastLogger.cpp

--- Miracast/common/MiracastLogger.cpp
+++ Miracast/common/MiracastLogger.cpp
@@ -33,7 +33,7 @@ namespace MIRACAST
             setvbuf(stdout, NULL, _IOLBF, 0);
     }
 
-    int gDefaultLogLevel = ERROR_LEVEL;
+    static int gDefaultLogLevel = ERROR_LEVEL;
     static std::string service_name = "NOT-DEFINED";
 
     void logger_init(const char* module_name)
