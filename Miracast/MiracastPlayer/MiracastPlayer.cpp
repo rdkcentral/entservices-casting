c8f763a5bd65d229f69672d634b1c977c6a06c65
preeja33 <preeja_raveendran@comcast.com>
2025-09-03 13:51:49 +0530

    Update MiracastPlayer.cpp

--- Miracast/MiracastPlayer/MiracastPlayer.cpp
+++ Miracast/MiracastPlayer/MiracastPlayer.cpp
@@ -133,27 +133,7 @@ namespace WPEFramework
             ASSERT(mCurrentService == service);
             ASSERT(0 == mConnectionId);
 
-			if (nullptr != mCurrentService)
-            {
-				LOGINFO("Entering Deintialize.!!!");
-                mCurrentService->Unregister(&mMiracastPlayerNotification);
-				mCurrentService->Release();
-				
-                if (mConfigure)
-                {   
-					LOGINFO("Entering mconfigure.!!!");
-                    uint32_t result = mConfigure->Configure(NULL);
-                    if (result == Core::ERROR_NONE) {
-                        SYSLOG(Logging::Shutdown, (string(_T("MiracastPlayer successfully destructed"))));
-                    }
-					LOGINFO("Entering mconfigure-> release.!!!");
-                    mConfigure->Release();
-                    mConfigure = NULL;
-					LOGINFO("After mconfigure-> release.!!!");
-		    	}
-                    
-						
-            }
+			
             if (nullptr != mMiracastPlayerImpl)
             {
 				LOGINFO("Entering mMiracastPlayerImpl .!!!");
@@ -190,6 +170,27 @@ namespace WPEFramework
                     connection->Release();
 					LOGINFO("Exiting mMiracastPlayerImpl .!!!");
                 }
+            }
+			if (nullptr != mCurrentService)
+            {
+				LOGINFO("Entering Deintialize.!!!");
+                mCurrentService->Unregister(&mMiracastPlayerNotification);
+				mCurrentService->Release();
+				
+                if (mConfigure)
+                {   
+					LOGINFO("Entering mconfigure.!!!");
+                    uint32_t result = mConfigure->Configure(NULL);
+                    if (result == Core::ERROR_NONE) {
+                        SYSLOG(Logging::Shutdown, (string(_T("MiracastPlayer successfully destructed"))));
+                    }
+					LOGINFO("Entering mconfigure-> release.!!!");
+                    mConfigure->Release();
+                    mConfigure = NULL;
+					LOGINFO("After mconfigure-> release.!!!");
+		    	}
+                    
+						
             }
 
             mConnectionId = 0;
