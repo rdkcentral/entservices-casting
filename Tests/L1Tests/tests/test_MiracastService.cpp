d2eff2fe3bda39114fadbcdc62b4f29af97fae0d
preeja33 <preeja_raveendran@comcast.com>
2025-09-04 17:53:33 +0530

    Update test_MiracastService.cpp

--- Tests/L1Tests/tests/test_MiracastService.cpp
+++ Tests/L1Tests/tests/test_MiracastService.cpp
@@ -245,10 +245,7 @@ protected:
 
     virtual ~MiracastServiceEventTest() override
     {
-		//Wait for all the previous destructor process to complete
-		//std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-
-        dispatcher->Deactivate();
+		dispatcher->Deactivate();
         dispatcher->Release();
 
         PluginHost::IFactories::Assign(nullptr);
