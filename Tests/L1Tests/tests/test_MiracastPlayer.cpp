17e1433a6dbe60345b0dd71eb2ce0002d284d2f1
balaji velmurugan <balaji_velmurugan@comcast.com>
2025-09-11 20:16:30 +0530

    RDKEMW-5823 : L1 test failure for MiracastService.
    Signed-off-by: balaji velmurugan <balaji_velmurugan@comcast.com>

--- Tests/L1Tests/tests/test_MiracastPlayer.cpp
+++ Tests/L1Tests/tests/test_MiracastPlayer.cpp
@@ -1,4 +1,3 @@
-#if 0
 /**
 * If not stated otherwise in this file or this component's LICENSE
 * file the following copyright and licenses apply:
@@ -290,12 +289,17 @@ namespace {
     bool initialize_ServerSocket(void)
     {
         // Create socket file descriptor
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
             perror("socket failed");
             return false;
         }
 
-        fcntl(server_fd, F_SETFL, O_NONBLOCK);
+        if (fcntl(server_fd, F_SETFL, O_NONBLOCK) < 0) {
+            perror("fcntl failed");
+            close(server_fd);
+            server_fd = -1;
+            return false;
+        }
         TEST_LOG("#### NON_BLOCKING Socket Enabled ####");
 
         // Forcefully attaching socket to the port 7236
@@ -381,7 +385,7 @@ namespace {
             std::string msg_buffer = "";
 
             memset( buffer , 0x00 , sizeof(buffer));
-            TEST_LOG("Index[%d] RTSP Msg[%x]",current_msg,rtsp_msg_type);
+            TEST_LOG("Index[%zu] RTSP Msg[%x]",current_msg,rtsp_msg_type);
 
             if ( RTSP_SEND == rtsp_sendorreceive )
             {
@@ -498,7 +502,7 @@ namespace {
                     case RTSP_RECV_M7_REQUEST:
                         {
                             TEST_LOG("RTSP_RECV REQUEST Messages");
-                            receivedCSeqNum = parse_received_parser_field_value( msg_buffer , "CSeq: " );
+                            receivedCSeqNum = std::move(parse_received_parser_field_value( msg_buffer , "CSeq: " ));
                         }
                         break;
                     default:
@@ -1310,4 +1314,3 @@ TEST_F(MiracastPlayerTest, SetOrUnsetEnvArguments)
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setEnvArguments"), _T("{\"envArgs\":[{\"argName\":\"DISPLAY\",\"argValue\":\"display-testplayer-0\"},{\"argName\":\"XDG_RUNTIME_DIR\",\"argValue\":\"/tmp\"}],\"appName\":\"MiracastApp\"}"), response));
     EXPECT_EQ(response, string("{\"message\":\"Failed, Missing Wayland Display Name\",\"success\":false}"));
 }
-#endif
