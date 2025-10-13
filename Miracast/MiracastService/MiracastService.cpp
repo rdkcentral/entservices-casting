ec991e4520ef842198079b81a5abb03e73a0aeda
Nairisnotavailable <120033603+Nairisnotavailable@users.noreply.github.com>
2025-10-13 18:59:34 +0530

    Update MiracastService.cpp to test

--- Miracast/MiracastService/MiracastService.cpp
+++ Miracast/MiracastService/MiracastService.cpp
@@ -133,23 +133,6 @@ namespace WPEFramework
             ASSERT(mCurrentService == service);
             ASSERT(0 == mConnectionId);
 
-			if (nullptr != mCurrentService)
-            {
-                /* Make sure the Activated and Deactivated are no longer called before we start cleaning up.. */
-				mCurrentService->Unregister(&mMiracastServiceNotification);
-				mCurrentService->Release();
-				mCurrentService = nullptr;
-
-                if (mConfigure)
-                {    
-                   uint32_t result = mConfigure->Configure(NULL);
-                   if (result == Core::ERROR_NONE) {
-                      SYSLOG(Logging::Shutdown, (string(_T("MiracastService successfully destructed"))));
-                   }
-                   mConfigure->Release();
-                   mConfigure = NULL;
-		    	}
-            }
             if (nullptr != mMiracastServiceImpl)
             {
                 if (mRegisterEvents)
@@ -161,7 +144,7 @@ namespace WPEFramework
 
                 /* Stop processing: */
                 RPC::IRemoteConnection* connection = nullptr;
-                if (service)
+                if (nullptr != service)
                 {
                     connection = service->RemoteConnection(mConnectionId);
                 }
@@ -184,6 +167,24 @@ namespace WPEFramework
                     connection->Terminate();
                     connection->Release();
                 }
+				
+				if (nullptr != mCurrentService)
+				{
+                /* Make sure the Activated and Deactivated are no longer called before we start cleaning up.. */
+				mCurrentService->Unregister(&mMiracastServiceNotification);
+				mCurrentService->Release();
+				mCurrentService = nullptr;
+
+					if (mConfigure)
+					{    
+						uint32_t result = mConfigure->Configure(NULL);
+						if (result == Core::ERROR_NONE) {
+						SYSLOG(Logging::Shutdown, (string(_T("MiracastService successfully destructed"))));
+						}
+						mConfigure->Release();
+						mConfigure = NULL;
+					}
+				}
             } 
             mConnectionId = 0;
             SYSLOG(Logging::Shutdown, (string(_T("MiracastService de-initialised"))));
