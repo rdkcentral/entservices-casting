d1ca5c4350a90f7204ab70736f3e0e1fadbee2b6
preeja33 <preeja_raveendran@comcast.com>
2025-12-30 14:37:21 +0530

    Update MiracastController.h

--- Miracast/MiracastService/MiracastController.h
+++ Miracast/MiracastService/MiracastController.h
@@ -25,6 +25,7 @@
 #include <fcntl.h>
 #include <algorithm>
 #include <vector>
+#include <mutex>
 #include <glib.h>
 #include <semaphore.h>
 #include "libIBus.h"
@@ -148,6 +149,7 @@ private:
     std::string m_localIp;
     vector<DeviceInfo *> m_deviceInfoList;
     GroupInfo *m_groupInfo;
+    std::mutex m_groupInfo_mutex;
     bool m_connectionStatus;
     bool m_p2p_backend_discovery{false};
     bool m_start_discovering_enabled{false};
