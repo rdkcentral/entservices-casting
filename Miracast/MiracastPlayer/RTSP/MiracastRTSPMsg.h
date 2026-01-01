5dd7849edc3ba2b65f8f3d45430cf8de2522b555
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2026-01-02 01:58:25 +0530

    Update MiracastRTSPMsg.h

--- Miracast/MiracastPlayer/RTSP/MiracastRTSPMsg.h
+++ Miracast/MiracastPlayer/RTSP/MiracastRTSPMsg.h
@@ -441,7 +441,6 @@ class MiracastRTSPMsg
 
         static MiracastRTSPMsg *getInstance(MiracastError &error_code , MiracastPlayerNotifier *player_notifier = nullptr , MiracastThread *controller_thread_id = nullptr);
         static void destroyInstance();
-        // NOTE: Pass by value required - threading API send_message() expects non-const void*
         void send_msgto_rtsp_msg_hdler_thread(RTSP_HLDR_MSGQ_STRUCT rtsp_hldr_msgq_data);
         void RTSPMessageHandler_Thread(void *args);
 
