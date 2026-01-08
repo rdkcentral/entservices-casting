6339b0acf68ce77207b8d96d7c75620057abe916
HarshiniRavichandran <Harshini_Ravichandran@comcast.com>
2026-01-08 12:28:04 +0530

    RDKEMW-11924 : Fix Coverity identified issues - entservices-casting
    Reason for Change: Coverity issues have been resolved with necessary code corrections.
    Test Procedure: Compiled and Verified
    Risks: Low
    Priority: P1
    version: minor
    
    Signed-off-by: HarshiniRavichandran <Harshini_Ravichandran@comcast.com>

--- Miracast/common/MiracastCommon.h
+++ Miracast/common/MiracastCommon.h
@@ -229,7 +229,8 @@ private:
 class MiracastCommon
 {
     public:
-        static std::string parse_opt_flag( std::string file_name , bool integer_check = false, bool debugStats = true );
+        // New fix : issue ID 30 : Update header declaration to match implementation using const reference
+        static std::string parse_opt_flag( const std::string& file_name , bool integer_check = false, bool debugStats = true );
         static int execute_SystemCommand( const char* system_command_buffer );
         static bool execute_PopenCommand( const char* popen_command, const char* expected_char, unsigned int retry_count, std::string& popen_buffer, unsigned int interval_micro_sec );
 };
