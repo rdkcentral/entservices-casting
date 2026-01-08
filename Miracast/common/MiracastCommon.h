18e6ca572ac7ae1468d44d3e8724e5ccb60a3c8a
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2026-01-08 18:57:56 +0530

    Update MiracastCommon.h

--- Miracast/common/MiracastCommon.h
+++ Miracast/common/MiracastCommon.h
@@ -229,8 +229,7 @@ private:
 class MiracastCommon
 {
     public:
-        // New fix : issue ID 585 : Update header declaration to match implementation using const reference (UNUSED_VALUE)
-        static std::string parse_opt_flag( const std::string& file_name , bool integer_check = false, bool debugStats = true );
+        static std::string parse_opt_flag( std::string file_name , bool integer_check = false, bool debugStats = true );
         static int execute_SystemCommand( const char* system_command_buffer );
         static bool execute_PopenCommand( const char* popen_command, const char* expected_char, unsigned int retry_count, std::string& popen_buffer, unsigned int interval_micro_sec );
 };
