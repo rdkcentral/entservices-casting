81bf5cf7788f1107a27c4ffa0ef8b46e818b313d
HarshiniRavichandran <Harshini_Ravichandran@comcast.com>
2026-01-08 12:43:04 +0530

    RDKEMW-11924 : Fix Coverity identified issues - entservices-casting
    Reason for Change: Coverity issues have been resolved with necessary code corrections.
    Test Procedure: Compiled and Verified
    Risks: Low
    Priority: P1
    version: minor
    
    Signed-off-by: HarshiniRavichandran <Harshini_Ravichandran@comcast.com>

--- Miracast/MiracastService/P2P/MiracastP2P.h
+++ Miracast/MiracastService/P2P/MiracastP2P.h
@@ -75,9 +75,11 @@ private:
     bool m_isWiFiDisplayParamsEnabled;
     pthread_t m_p2p_ctrl_monitor_thread_id;
 
-    MiracastError p2pInit(std::string p2p_ctrl_iface);
+    // New fix : issue ID 37 : Update header declaration to match implementation using const reference
+    MiracastError p2pInit(const std::string& p2p_ctrl_iface);
     MiracastError p2pUninit();
-    MiracastError executeCommand(std::string command, int interface, std::string &retBuffer);
+    // New fix : issue ID 38 : Update header declaration to match implementation using const reference
+    MiracastError executeCommand(const std::string& command, int interface, std::string &retBuffer);
     int p2pExecute(char *cmd, enum INTERFACE iface, char *ret_buf, size_t actual_buffer_len);
     int p2pWpaCtrlSendCmd(char *cmd, struct wpa_ctrl *wpa_p2p_ctrl_iface, char *ret_buf,size_t actual_buffer_len);
     void Release_P2PCtrlInterface(void);
