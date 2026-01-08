dffcf1f8d7d2090e9b0ddce1f7aafd6c5e0ed462
HarshiniRavichandran <166796069+HarshiniRavichandran@users.noreply.github.com>
2026-01-08 18:56:52 +0530

    Update MiracastCommon.cpp

--- Miracast/common/MiracastCommon.cpp
+++ Miracast/common/MiracastCommon.cpp
@@ -147,8 +147,7 @@ int8_t MiracastThread::receive_message(void *message, size_t msg_size, int sem_w
     return status;
 }
 
-// New fix : issue ID 585 : Use const reference to avoid unnecessary copy of file_name string object (UNUSED_VALUE)
-std::string MiracastCommon::parse_opt_flag( const std::string& file_name , bool integer_check , bool debugStats )
+std::string MiracastCommon::parse_opt_flag( std::string file_name , bool integer_check , bool debugStats )
 {
     std::string return_buffer = "";
     std::ifstream parse_opt_flag_file( file_name.c_str());
